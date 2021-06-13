bc514410ce9da499c1192a6bf3defe6229e77f7d
Berreta <felipeberreta@hotmail.com>
2021-06-13 16:52:22 -0300

    Segundo MVP

--- app/src/main/java/com/devventure/dadosdasorte/MainActivity.kt
+++ app/src/main/java/com/devventure/dadosdasorte/MainActivity.kt
@@ -3,26 +3,26 @@ package com.devventure.dadosdasorte
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dado01 = findViewById<TextView>(R.id.Dado1)
-        val dado02 = findViewById<TextView>(R.id.Dado2)
+        val dado01 = findViewById<ImageView>(R.id.Dado01)
+        val dado02 = findViewById<ImageView>(R.id.Dado02)
         val btn = findViewById<Button>(R.id.btnJogar)
 
+        val imagensDadoSeisLados = listOf(R.drawable.dice_1,R.drawable.dice_2,
+                                        R.drawable.dice_3,R.drawable.dice_4,
+                                        R.drawable.dice_5,R.drawable.dice_6)
 
-        btn.setOnClickListener() {
-            dado01.text =  randomNumber().toString()
-            dado02.text = randomNumber().toString()
+        btn.setOnClickListener {
+            dado01.setImageResource(imagensDadoSeisLados.random())
+            dado02.setImageResource(imagensDadoSeisLados.random())
         }
-
     }
 
-    private fun randomNumber() : Int {
-        return (1..6).random()
-    }
+    //private fun randomNumber() : Int {  return (1..6).random() }
 }
